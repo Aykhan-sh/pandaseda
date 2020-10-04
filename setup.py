a8f8f99929e85fa0cfd5619f0612358ff7ea6f84
Aykhan <a.turlasov@gmail.com>
2020-10-05 01:25:34 +0600

    package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PandasEDA", # Replace with your own username
+    name="pandaseda", # Replace with your own username
     version="0.0.1",
     author="Aykhan-sh",
     author_email="a.turlasov@gmail.com",
