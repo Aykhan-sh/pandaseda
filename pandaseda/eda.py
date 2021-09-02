3f693fe3399aff63ef8c3049dd3b04d9381abbaa
Renat Alimbekov <alimbekovr@hotmail.com>
2021-09-02 17:49:53 +0600

    Update eda.py

--- pandaseda/eda.py
+++ pandaseda/eda.py
@@ -5,24 +5,3 @@ from IPython.core.display import HTML, display
 from math import ceil
 
 
-def distplots(df, columns, hue=None, subplots_params=None):
-    if isinstance(hue, str) or hue is None:
-        hue = [hue]*len(columns)
-    s_params = {
-        'nrows': len(columns),
-        'ncols': 1,
-        'figsize': (15, len(columns) * 8)}
-    if subplots_params is not None:
-        s_params.update(subplots_params)
-        if 'figsize' not in subplots_params:
-            s_params['figsize'] = (s_params['nrows'] * 10, s_params['ncols'] * 8)
-
-    fig, axis = plt.subplots(**s_params)
-    display(HTML('<h1><B><center>' f"Distribution plots" "</span></h1>"))
-    ax = axis.ravel()
-    for idx, i in enumerate(columns):
-        sns.histplot(x=i, data=df.dropna(subset=[i]), ax=ax[idx], hue=hue[idx])
-        ax[idx].tick_params(labelsize=14)
-        ax[idx].set_xlabel('')
-        ax[idx].set_title(i, fontsize=23)
-    plt.tight_layout()
