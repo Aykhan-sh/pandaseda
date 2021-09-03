f3ea512cd26f9b0a667846c905c5033207ce1692
Renat Alimbekov <alimbekovr@hotmail.com>
2021-09-03 11:12:31 +0600

    Update eda.py

--- pandaseda/eda.py
+++ pandaseda/eda.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 from IPython.core.display import HTML, display
 from math import ceil
-from typing import List, Tuple, Dict, AnyStr 
+from typing import List, Tuple, Dict, AnyStr, Any
 
 class Describe:
     def __init__(self, df: pd.DataFrame) -> None:
